25b661c70c151a8cf08ecfe247a375bfc001cc6d
aalissy <32420245+aalissy@users.noreply.github.com>
2022-09-20 20:45:49 -0500

    Added comments

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,4 @@
+// If user isn't logged in redirects to the login page otherwise lets the user continue
 const withAuth = (req, res, next) => {
   if (!req.session.logged_in) {
     res.redirect('/login');
