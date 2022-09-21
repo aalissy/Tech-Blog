cf7c7a6b44d3dbcdf50134eded72db52d795e92c
aalissy <32420245+aalissy@users.noreply.github.com>
2022-09-20 20:37:05 -0500

    Added comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+// Requires and uses the apiRoutes, homeRoutes, and dashboardRoutes
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
