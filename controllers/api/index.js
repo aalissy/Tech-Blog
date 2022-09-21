911b3a15a174ef6323150c3d9429748fe5f5c8ee
aalissy <32420245+aalissy@users.noreply.github.com>
2022-09-20 20:33:31 -0500

    Added comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,4 @@
+// Requires and uses the userRoutes, postRoutes, and commentRoutes
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
