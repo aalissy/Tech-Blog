eeb011a6095d59616a39f48a07e63c8a8484a295
aalissy <32420245+aalissy@users.noreply.github.com>
2022-09-20 20:40:02 -0500

    Added comments

--- models/index.js
+++ models/index.js
@@ -2,29 +2,35 @@ const User = require('./User');
 const Post= require('./Post');
 const Comment = require('./Comment');
 
+// User has many Posts
 User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Post.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE'
-});
-
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
-});
-
+// User has Many Comments
 User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+// Posts belongs to User
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// Post has many Comments
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+// Comments belongs to Post
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+// Comments belongs to User
 Comment.belongsTo(User, {
   foreignKey: 'user_id'
 })
